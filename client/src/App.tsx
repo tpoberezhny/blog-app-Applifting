73ecbf1d173d929f1acacac8907f118f600a9c28
Tim <poberezhny003@gmail.com>
2024-11-23 13:55:14 +0100

    Configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,22 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Login from './pages/Login';
+import './App.css';
 
-function App() {
-
+const App: React.FC = () => {
   return (
-    <main>
-      <div>Hello</div>
-    </main>
-  )
-}
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <NavBar />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<div>Home Page</div>} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
