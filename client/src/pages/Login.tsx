c9dfcf0e11a4f876be56405aace1e4cfb928ec14
Tim <poberezhny003@gmail.com>
2024-11-23 13:55:58 +0100

    Login Page

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../redux/slices/authSlice";
+import { RootState, AppDispatch } from "../redux/store";
+import { useNavigate } from "react-router-dom";
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+  const { isAuthenticated, error } = useSelector(
+    (state: RootState) => state.auth
+  );
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    dispatch(loginUser({ email, password }));
+  };
+
+  if (isAuthenticated) {
+    navigate("/");
+  }
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white w-96 p-8 rounded-md shadow-lg"
+      >
+        <h2 className="text-2xl font-semibold mb-6 text-center">Log In</h2>
+        {error && <p className="text-red-600 mb-4 text-center">{error}</p>}
+        <div className="mb-4">
+          <label className="block text-gray-700 mb-2">Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 mb-2">Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition duration-200"
+        >
+          Log In
+        </button>
+        <div className="mt-4">
+          <h1 className="text-2xl">For succesful log in use those credentials:</h1>
+          <div className="mt-4  bg-red-500">
+          <h2 className="text-xl">Email: test@test.com</h2>
+          <h2 className="text-xl">Password: test</h2>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
